d99f14522b7ec6cffc405c72daf152d895498034
Bruno Binet <bruno.binet@gmail.com>
2012-03-28 01:20:15 +0200

    move swatch stuff to a resources directory

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ BASEDIR=$(cd $(dirname $0) && pwd)
 LESSC=$BASEDIR/less.js/bin/lessc
 
 ( cd $BASEDIR
-    $LESSC ./swatchmaker.less > swatch/bootstrap.css
-    $LESSC --compress  ./swatchmaker.less > swatch/bootstrap.min.css
+    $LESSC ./resources/swatchmaker.less > resources/swatch/bootstrap.css
+    $LESSC --compress  ./resources/swatchmaker.less > resources/swatch/bootstrap.min.css
 )
