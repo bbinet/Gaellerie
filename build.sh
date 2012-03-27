10419df51e4347cd22583ed42ff22bf2f6b81a2d
Bruno Binet <binet.bruno@gmail.com>
2012-03-28 01:06:08 +0200

    add own bootswatch based on cyborg theme: http://bootswatch.com/cyborg/

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(cd $(dirname $0) && pwd)
 LESSC=$BASEDIR/less.js/bin/lessc
 
-(cd $BASEDIR/bootswatch/swatchmaker/ && ln -sf ../../bootstrap/ ./)
-
-(cd $BASEDIR/bootswatch/swatchmaker/ && $LESSC ./swatchmaker.less > swatch/bootstrap.css)
-(cd $BASEDIR/bootswatch/swatchmaker/ && $LESSC --compress  ./swatchmaker.less > swatch/bootstrap.min.css)
+( cd $BASEDIR
+    $LESSC ./swatchmaker.less > swatch/bootstrap.css
+    $LESSC --compress  ./swatchmaker.less > swatch/bootstrap.min.css
+)
