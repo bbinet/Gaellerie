36844f062da76825923e3f8bfdcf84722e2803eb
Bruno Binet <bruno.binet@gmail.com>
2012-03-29 21:46:07 +0200

    update gh-pages

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASEDIR=$(cd $(dirname $0) && pwd)
+LESSC=$BASEDIR/less.js/bin/lessc
+
+( cd $BASEDIR
+    $LESSC ./resources/swatchmaker.less > resources/swatch/bootstrap.css
+    $LESSC --compress ./resources/swatchmaker.less > resources/swatch/bootstrap.min.css
+
+    rsync -avz --cvs-exclude  --delete \
+        --exclude="_build" \
+        --exclude=".*" \
+        --exclude="docs" \
+        --exclude="tests" \
+        --exclude="less.js" \
+        . _build/
+)
