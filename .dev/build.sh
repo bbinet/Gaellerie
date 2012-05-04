c46aeb629a573cde9ed8b7a46bde5a90a54e5212
Bruno Binet <bruno.binet@gmail.com>
2012-05-05 01:36:23 +0200

    fix build.sh script (and comment out rsync stuff)

--- .dev/build.sh
+++ .dev/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 BASEDIR=$(cd $(dirname $0) && pwd)
-LESSC=./less.js/bin/lessc
+LESSC=./submodules/less.js/bin/lessc
 
 ( cd "${BASEDIR}"
-    $LESSC ./resources/swatchmaker.less > resources/swatch/bootstrap.css
-    $LESSC --compress ./resources/swatchmaker.less > resources/swatch/bootstrap.min.css
+    $LESSC --compress ./swatchmaker.less > ../static/css/bootstrap.min.css
 
-    rsync -avz --cvs-exclude  --delete \
-        --exclude="_build" \
-        --exclude=".*" \
-        --exclude="docs" \
-        --exclude="tests" \
-        --exclude="less.js" \
-        . _build/
+    # rsync -avz --cvs-exclude  --delete \
+    #     --exclude="_build" \
+    #     --exclude=".*" \
+    #     --exclude="docs" \
+    #     --exclude="tests" \
+    #     --exclude="less.js" \
+    #     . _build/
 )
