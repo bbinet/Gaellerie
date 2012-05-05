1014c4f61b99cc61135b73f0c691224ae0028d83
Bruno Binet <bruno.binet@gmail.com>
2012-05-05 02:31:51 +0200

    update .dev/build.sh to minify galleria.js and copy galleria resources to static dir

--- .dev/build.sh
+++ .dev/build.sh
@@ -5,6 +5,8 @@ LESSC=./submodules/less.js/bin/lessc
 
 ( cd "${BASEDIR}"
     $LESSC --compress ./swatchmaker.less > ../static/css/bootstrap.min.css
+    ./submodules/UglifyJS/bin/uglifyjs ./submodules/galleria/src/galleria.js > ../static/js/galleria.js
 
     rsync -avz --cvs-exclude ./static/* ../static
+    rsync -avz --cvs-exclude ./submodules/galleria/src/themes/* ../static/galleria-themes
 )
