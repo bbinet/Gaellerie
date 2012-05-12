0af648f20f262b998ebf3c9d07a63b50fb02c3db
Bruno Binet <bruno.binet@gmail.com>
2012-05-12 15:17:11 +0200

    update build.sh script

--- .dev/build.sh
+++ .dev/build.sh
@@ -4,12 +4,19 @@ BASEDIR=$(cd $(dirname $0) && pwd)
 LESSC=./submodules/less.js/bin/lessc
 
 ( cd "${BASEDIR}"
-    $LESSC --compress ./swatchmaker.less > ../static/css/bootstrap.min.css
-    cat ./submodules/galleria/src/galleria.js \
-        ./submodules/galleria/src/plugins/flickr/galleria.flickr.js | \
-        ./submodules/UglifyJS/bin/uglifyjs > ../static/js/galleria.js
 
+    echo "> Concatenate and build less/css to ../static/css/all.min.css"
+    $LESSC --compress ./swatchmaker.less > ../static/css/all.min.css
+
+    echo "> Concatenate and build js to ../static/js/all.min.js"
+    cat ./static/js/jquery-1.7.1.js \
+        ./submodules/galleria/src/galleria.js \
+        ./submodules/galleria/src/plugins/flickr/galleria.flickr.js | \
+        ./submodules/UglifyJS/bin/uglifyjs > ../static/js/all.min.js
+
+    echo "> Copy bootstrap/img/ files to ../static/img/"
     rsync -avz --cvs-exclude ./submodules/bootstrap/img/* ../static/img
-    rsync -avz --cvs-exclude ./static/* ../static
+
+    echo "> Copy galleria/src/themes files to ../static/galleria-themes/"
     rsync -avz --cvs-exclude ./submodules/galleria/src/themes/* ../static/galleria-themes
 )
